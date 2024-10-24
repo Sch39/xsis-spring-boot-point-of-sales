2183e8c8c39248dbf7f862154e2cbeb0c9347591
Sch39 <sukronraezpactoer@gmail.com>
2024-10-25 05:32:52 +0700

    Fix warning about page serialization via dto

---
+++ src/main/java/dev/sch39/ecommerce/config/WebConfig.java
@@ -0,0 +1,10 @@
+package dev.sch39.ecommerce.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+@Configuration
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
+public class WebConfig {
+
+}

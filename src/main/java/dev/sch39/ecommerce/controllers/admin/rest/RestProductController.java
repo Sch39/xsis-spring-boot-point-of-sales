59f3f7aea2dadd836527134df51755d15c2f8b57
Sch39 <sukronraezpactoer@gmail.com>
2024-10-24 10:09:03 +0700

    Add rest admin get with filter in product

--- src/main/java/dev/sch39/ecommerce/controllers/admin/rest/RestProductController.java
+++ src/main/java/dev/sch39/ecommerce/controllers/admin/rest/RestProductController.java
@@ -1,14 +1,18 @@
 package dev.sch39.ecommerce.controllers.admin.rest;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import dev.sch39.ecommerce.dtos.rest.request.RestProductAdminFilterRequestDto;
 import dev.sch39.ecommerce.dtos.rest.request.RestProductRequestdto;
 import dev.sch39.ecommerce.dtos.rest.response.RestProductAdminResponseDto;
 import dev.sch39.ecommerce.services.rest.RestProductService;
@@ -45,4 +49,22 @@ public class RestProductController {
     }
   }
 
+  @GetMapping({ "", "/" })
+  public ResponseEntity<ApiResponse> getProducts(RestProductAdminFilterRequestDto filterRequestDto) {
+    try {
+      List<RestProductAdminResponseDto> responseDtos = restProductService.getProductsForAdminByFilter(filterRequestDto);
+
+      SuccessApiResponse<List<RestProductAdminResponseDto>> apiResponse = new SuccessApiResponse<>();
+      apiResponse.setSuccess(true);
+      apiResponse.setMessage("Success get all product");
+      apiResponse.setData(responseDtos);
+
+      return new ResponseEntity<>(apiResponse, HttpStatus.OK);
+    } catch (Exception e) {
+      ErrorApiResponse<Void> errorApiResponse = new ErrorApiResponse<>();
+      errorApiResponse.setSuccess(false);
+      errorApiResponse.setMessage(e.getMessage());
+      return new ResponseEntity<>(errorApiResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
 }

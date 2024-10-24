6bfc558eaf6374fcf42899b511805984751bbb27
Sch39 <sukronraezpactoer@gmail.com>
2024-10-24 14:48:01 +0700

    complete rest api variant for admin

--- src/main/java/dev/sch39/ecommerce/controllers/admin/rest/RestVariantController.java
+++ src/main/java/dev/sch39/ecommerce/controllers/admin/rest/RestVariantController.java
@@ -1,12 +1,18 @@
 package dev.sch39.ecommerce.controllers.admin.rest;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import dev.sch39.ecommerce.dtos.rest.request.RestVariantAdminFilterRequestDto;
 import dev.sch39.ecommerce.dtos.rest.request.RestVariantAdminRequestDto;
 import dev.sch39.ecommerce.dtos.rest.response.RestVariantAdminResponseDto;
 import dev.sch39.ecommerce.services.rest.RestVariantService;
@@ -15,6 +21,7 @@ import dev.sch39.ecommerce.utils.ErrorApiResponse;
 import dev.sch39.ecommerce.utils.SuccessApiResponse;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @CrossOrigin("*")
@@ -45,4 +52,84 @@ public class RestVariantController {
     }
   }
 
+  @GetMapping({ "", "/" })
+  public ResponseEntity<ApiResponse> getVariants(RestVariantAdminFilterRequestDto filterRequestDto) {
+    try {
+      List<RestVariantAdminResponseDto> responseDtos = restVariantService.getVariantsForAdminByFilter(filterRequestDto);
+
+      SuccessApiResponse<List<RestVariantAdminResponseDto>> apiResponse = new SuccessApiResponse<>();
+      apiResponse.setSuccess(true);
+      apiResponse.setMessage("Succes get all product");
+      apiResponse.setData(responseDtos);
+
+      return new ResponseEntity<>(apiResponse, HttpStatus.OK);
+    } catch (Exception e) {
+      ErrorApiResponse<Void> errorApiResponse = new ErrorApiResponse<>();
+      errorApiResponse.setSuccess(false);
+      errorApiResponse.setMessage(e.getMessage());
+      return new ResponseEntity<>(errorApiResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  @GetMapping({ "{id}", "{id}/" })
+  public ResponseEntity<ApiResponse> getVariantById(@PathVariable("id") Long id) {
+    try {
+      RestVariantAdminResponseDto responseDto = restVariantService.getVariantByIdForAdmin(id);
+
+      SuccessApiResponse<RestVariantAdminResponseDto> apiResponse = new SuccessApiResponse<>();
+      apiResponse.setSuccess(true);
+      apiResponse.setMessage("Success get product");
+      apiResponse.setData(responseDto);
+
+      return new ResponseEntity<>(apiResponse, HttpStatus.OK);
+    } catch (Exception e) {
+      ErrorApiResponse<Void> errorApiResponse = new ErrorApiResponse<>();
+      errorApiResponse.setSuccess(false);
+      errorApiResponse.setMessage(e.getMessage());
+      return new ResponseEntity<>(errorApiResponse,
+          HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  @PutMapping({ "{id}", "{id}/" })
+  public ResponseEntity<ApiResponse> updateVariantById(@PathVariable("id") Long id,
+      @RequestBody RestVariantAdminRequestDto requestDto) {
+    try {
+      RestVariantAdminResponseDto responseDto = restVariantService.updateVariantByIdForAdmin(id, requestDto);
+
+      SuccessApiResponse<RestVariantAdminResponseDto> apiResponse = new SuccessApiResponse<>();
+      apiResponse.setSuccess(true);
+      apiResponse.setMessage("Updated product successfully");
+      apiResponse.setData(responseDto);
+
+      return new ResponseEntity<>(apiResponse, HttpStatus.OK);
+    } catch (Exception e) {
+      ErrorApiResponse<Void> errorApiResponse = new ErrorApiResponse<>();
+      errorApiResponse.setSuccess(false);
+      errorApiResponse.setMessage(e.getMessage());
+
+      return new ResponseEntity<>(errorApiResponse,
+          HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  @DeleteMapping({ "{id}", "{id}/" })
+  public ResponseEntity<ApiResponse> deleteVariantById(@PathVariable("id") Long id) {
+    try {
+      restVariantService.deleteVariantByIdForAdmin(id);
+
+      ApiResponse apiResponse = new ApiResponse();
+      apiResponse.setSuccess(true);
+      apiResponse.setMessage("Product deleted successfully");
+
+      return new ResponseEntity<>(apiResponse, HttpStatus.OK);
+    } catch (Exception e) {
+      ErrorApiResponse<Void> errorApiResponse = new ErrorApiResponse<>();
+      errorApiResponse.setSuccess(false);
+      errorApiResponse.setMessage(e.getMessage());
+
+      return new ResponseEntity<>(errorApiResponse,
+          HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
 }

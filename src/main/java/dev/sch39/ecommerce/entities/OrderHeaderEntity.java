8313abc145fb3eb85005b5e7aec7dd7846542718
Sch39 <sukronraezpactoer@gmail.com>
2024-10-25 11:26:07 +0700

    Add user relationship and order status in order header

--- src/main/java/dev/sch39/ecommerce/entities/OrderHeaderEntity.java
+++ src/main/java/dev/sch39/ecommerce/entities/OrderHeaderEntity.java
@@ -2,11 +2,16 @@ package dev.sch39.ecommerce.entities;
 
 import org.hibernate.annotations.SQLDelete;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -23,6 +28,23 @@ public class OrderHeaderEntity extends BaseEntity {
 
   private String reference;
   private Double amount;
+
+  @ManyToOne(cascade = CascadeType.ALL)
+  @JoinColumn(name = "user_id", insertable = false, updatable = false)
+  private UserEntity user;
+
+  @Column(name = "user_id")
+  private Long userId;
+
+  @Enumerated(EnumType.STRING)
+  private OrderHeaderStatus orderStatus = OrderHeaderStatus.PENDING;
+
   @Column(name = "is_deleted")
-  private boolean deleted;
+  private boolean deleted = false;
+
+  public enum OrderHeaderStatus {
+    PENDING,
+    SUCCESS,
+    FAILED;
+  }
 }
